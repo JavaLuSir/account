547365985e12401c2c6f49c99a0006aab62de5f1
JavaLuSir <javalusir@163.com>
2026-02-25 15:46:08 +0800

    Change back to MySQL database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+-- 初始化测试数据
+INSERT INTO T_ACCOUNT (PROP, OWNER, ACCNAME, ACCOUNT, BALANCE, MTYPE, REMARK, OPERATER) VALUES
+('1', '张三', '现金', 'CASH001', 5000.00, '现金', '手持现金', 'admin'),
+('1', '张三', '中国银行', 'BOC123456', 15000.00, '储蓄卡', '中国银行储蓄卡', 'admin'),
+('1', '张三', '支付宝', 'ALIPAY001', 8000.00, '第三方支付', '支付宝账户', 'admin'),
+('1', '张三', '微信钱包', 'WECHAT001', 3000.00, '第三方支付', '微信支付', 'admin'),
+('2', '张三', '信用卡', 'CREDIT001', -5000.00, '信用卡', '招商银行信用卡', 'admin'),
+('2', '张三', '花呗', 'HUABEI001', -2000.00, '消费信贷', '支付宝花呗', 'admin');
+
+INSERT INTO T_WATER (AID, TRDATE, TRADEKIND, TRTYPE, TRNUM, REMARK, OPERATER) VALUES
+(1, CURDATE(), '1', '1', 3000.00, '工资收入', 'admin'),
+(2, CURDATE(), '0', '1', 15000.00, '工资入账', 'admin'),
+(3, CURDATE(), '2', '1', 500.00, '退款', 'admin'),
+(1, CURDATE(), '3', '0', 200.00, '交通出行', 'admin'),
+(3, CURDATE(), '2', '0', 150.00, '日常消费', 'admin'),
+(5, CURDATE(), '5', '0', 2000.00, '信用卡还款', 'admin');
